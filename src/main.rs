7f11aab6fd077eb4d9f693f3b69046cfe4e970bb
sebastiano <seba.g90@gmail.com>
2025-03-05 16:35:49 +0100

    remove comment

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use walkdir::WalkDir;
 
 fn dstroy_hook(path: &str) -> io::Result<()> {
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok())
-    // skip files without permissions
     {
         if entry.file_name() == ".DS_Store" {
             fs::remove_file(&entry.path())?;
